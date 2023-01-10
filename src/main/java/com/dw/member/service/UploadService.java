41409f31ef709f3aa1e32210e2c2731d9b6e0d3f
hyunsangwon <hyunsangwon93@gmail.com>
2023-01-10 15:42:59 +0900

    feat : S3 사진 업로드 끝

--- src/main/java/com/dw/member/service/UploadService.java
+++ src/main/java/com/dw/member/service/UploadService.java
@@ -17,9 +17,14 @@ public class UploadService {
         String today = new SimpleDateFormat("yyMMdd").format(new Date());
         String imageBasePath = "https://hyunsangwon.pro.s3.ap-northeast-2.amazonaws.com";//디비에 저장할 때 사용
         String imagePath = "/upload/"+today; //이미지를 저장할 폴더
-        String imageName = locationFile[0].getName()+today+".jpg";
+        //실무에서는 사진이름을 현재시각(초 까지) 혹은 PK 이름으로 저장한다.
+        //사진 용량도 코딩으로 줄여서 저장한다. (image resize)
+        String imageName = "hyunsangwon"+locationFile[0].getName()+".jpg";
         try{
-            awsService.uploadObject(locationFile[0],imagePath,imageName);
+            awsService.uploadObject(locationFile[0],imagePath,imageName); //S3에 이미지 업로드
+            String s3Url = imageBasePath + imagePath + "/" +imageName;
+            //s3Url를 DB에 저장하는 로직을 구현하면 끝
+
         }catch(Exception e){
             return false;
         }
