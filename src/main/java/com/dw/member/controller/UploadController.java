9d02dc96c3736aa36b4899e1b22f276b050393f0
hyunsangwon <hyunsangwon93@gmail.com>
2023-01-10 14:36:14 +0900

    feat : 사진업로드 완성

--- src/main/java/com/dw/member/controller/UploadController.java
+++ src/main/java/com/dw/member/controller/UploadController.java
@@ -1,23 +1,28 @@
 package com.dw.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.dw.member.service.UploadService;
+
 @RestController
 public class UploadController {
     
+
+    @Autowired
+    private UploadService uploadService;
+
     //MultipartFile 클래스로 사진을 전송 받는다.
     @PostMapping("/api/v1/image")
     public boolean uploadImage(@RequestParam("image") MultipartFile[] locationFile){
 
         System.out.println("사진 이름은 : "+locationFile[0].getName());
         System.out.println("사진 사이즈 : "+locationFile[0].getSize());
-
-
         
-        return true;
+        return uploadService.uploadImageToAwsS3(locationFile);
     }
 
 }
--- src/main/java/com/dw/member/service/UploadService.java
+++ src/main/java/com/dw/member/service/UploadService.java
@@ -17,7 +17,7 @@ public class UploadService {
         String today = new SimpleDateFormat("yyMMdd").format(new Date());
         String imageBasePath = "https://hyunsangwon.pro.s3.ap-northeast-2.amazonaws.com";//디비에 저장할 때 사용
         String imagePath = "/upload/"+today; //이미지를 저장할 폴더
-        String imageName = locationFile[0].getName()+".jpg";
+        String imageName = locationFile[0].getName()+today+".jpg";
         try{
             awsService.uploadObject(locationFile[0],imagePath,imageName);
         }catch(Exception e){
