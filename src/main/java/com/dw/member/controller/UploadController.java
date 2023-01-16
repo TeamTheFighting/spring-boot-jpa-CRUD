ac710cb407c999be2dbcacb1cc7bc13a74b4a028
hyunsangwon <hyunsangwon93@gmail.com>
2023-01-16 15:05:16 +0900

    업로드 수정

--- src/main/java/com/dw/member/controller/UploadController.java
+++ src/main/java/com/dw/member/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.dw.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.dw.member.service.UploadService;
 
 @RestController
+@CrossOrigin
 public class UploadController {
     
 
